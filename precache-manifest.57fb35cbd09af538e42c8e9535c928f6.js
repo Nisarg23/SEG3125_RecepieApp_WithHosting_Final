d29f02241350139bb64777362acd3a1b98528712
Nisarg Patel <npate112@uottawa.ca>
2022-07-20 18:55:05 -0400

    Updates

---
+++ precache-manifest.57fb35cbd09af538e42c8e9535c928f6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439017c5d7766b7afe7174ecf0de7f34",
+    "url": "/SEG3125_RecepieApp_WithHosting_Final/index.html"
+  },
+  {
+    "revision": "1af3fdfb7558f34b8783",
+    "url": "/SEG3125_RecepieApp_WithHosting_Final/static/css/main.6643539f.chunk.css"
+  },
+  {
+    "revision": "c855fb5d91c6f03367aa",
+    "url": "/SEG3125_RecepieApp_WithHosting_Final/static/js/2.1bdbc820.chunk.js"
+  },
+  {
+    "revision": "34d78359945782a7f751c9a3928a8268",
+    "url": "/SEG3125_RecepieApp_WithHosting_Final/static/js/2.1bdbc820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1af3fdfb7558f34b8783",
+    "url": "/SEG3125_RecepieApp_WithHosting_Final/static/js/main.7cf8a0da.chunk.js"
+  },
+  {
+    "revision": "dbdcb51f22cfb50b8f11",
+    "url": "/SEG3125_RecepieApp_WithHosting_Final/static/js/runtime-main.46a95650.js"
+  },
+  {
+    "revision": "32466b5c27c62c80c46cbe41c4259056",
+    "url": "/SEG3125_RecepieApp_WithHosting_Final/static/media/background.32466b5c.jpg"
+  },
+  {
+    "revision": "ad724c4ebc16093ff5a7ff5944f91e01",
+    "url": "/SEG3125_RecepieApp_WithHosting_Final/static/media/flour.ad724c4e.png"
+  },
+  {
+    "revision": "1185e868dd5d467843a9fc7936569ef5",
+    "url": "/SEG3125_RecepieApp_WithHosting_Final/static/media/logo.1185e868.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/SEG3125_RecepieApp_WithHosting_Final/precache-manifest.57fb35cbd09af538e42c8e9535c928f6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SEG3125_RecepieApp_WithHosting_Final/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
